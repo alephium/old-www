0c3d34b5595b563998a8204bd88fe9ba7795ab93
Mikael Vaivre <m.vaivre@gmail.com>
2019-10-21 11:18:10 +0200

    Complete Roadmap layout

--- src/sections/roadmap/RoadmapSection.tsx
+++ src/sections/roadmap/RoadmapSection.tsx
@@ -13,8 +13,33 @@ const RoadmapSection: React.FC = () => {
 						side={Side.left}
 						stepNumber={1}
 						title='2018 Q2 - Q4 '
-						text='The Alpha version including the core BlockFlow algorithm was implemented. Alpha version tested on AWS achieving > 10K TPS'
-					/>
+						done
+					>
+						The Alpha version including the core BlockFlow algorithm was implemented. Alpha version tested on AWS achieving > 10K TPS
+					</RoadmapStep>
+					<RoadmapStep 
+						side={Side.right}
+						stepNumber={2}
+						title='2018 Q2 - Q4 '
+						done
+					>
+						Alephium's testnet 1.0 will be released with beta applications for participants.
+					</RoadmapStep>
+					<RoadmapStep 
+						side={Side.left}
+						stepNumber={3}
+						title='2018 Q2 - Q4 '
+					>
+						The testnet would be further enhanced with performance, security, and protocol improments.
+					</RoadmapStep>
+					<RoadmapStep 
+						side={Side.right}
+						stepNumber={4}
+						title='After Pre-A Funding '
+						highlight
+					>
+						The initial <mark>main net</mark> will be released.
+					</RoadmapStep>
 				</div>
 			</div>
 		</section>
@@ -30,23 +55,27 @@ interface RoadmapStepProps {
 	side: Side
 	stepNumber: number
 	title: string
-	text: string
 	done?: boolean
 	highlight?: boolean
 }
 
-const RoadmapStep: React.FC<RoadmapStepProps> = ({ side, stepNumber, title, text, done, highlight}) => {
+const RoadmapStep: React.FC<RoadmapStepProps> = ({ side, stepNumber, title, done, highlight, children }) => {
 	return (
-		<div className={`RoadmapStep ${side.toString}`}>
-			<div className='box'>
-				<h1 className='title'>{title}</h1>
-				<p className='text'>{text}</p>
+		<div className={`RoadmapStep ${Side[side]}`}>
+			<div className='content'>
+				<div className='box'>
+					<h1 className='title'>{title}</h1>
+					<p className='text'>{children}</p>
+				</div>
+				<div className='link'/>
+				<div className='number-box'>
+					<div className='number'>{stepNumber}</div>
+					<div className={`notification-bubble ${done ? 'done' : ''} ${highlight ? 'highlight' : ''}`}>
+						<div className='notification-bubble__icon' />
+					</div>
+				</div>
 			</div>
-			<div className='link'/>
-			<div className='number-box'>
-				<div className='number'>{stepNumber}</div>
-				<div className={`notification-bubble ${done ? 'done' : ''} ${highlight ? 'highlight' : ''}`} />
-			</div>
+			<div className='free-space' />
 		</div>
 	)
 }
