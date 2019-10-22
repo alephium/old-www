fb0ae9d5726011e1cc35891e616b7077cb93fa77
Mikael Vaivre <m.vaivre@gmail.com>
2019-10-22 10:52:07 +0200

    First example of staggering children into view with Framer Motion

--- src/sections/title/TitleSection.tsx
+++ src/sections/title/TitleSection.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useRef, useLayoutEffect } from 'react';
 import './TitleSection.css';
+import { motion, useViewportScroll, useTransform } from "framer-motion"
 
-const TitleSection: React.FC = () => {
+const TitleSection = () => {
+
+	const [elementTop, setElementTop] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollY } = useViewportScroll();
+
+  const y = useTransform(scrollY, [elementTop -1, elementTop + 1], [0, -0.2], {
+    clamp: false
+	});
+	console.log(elementTop)
+
+  useLayoutEffect(() => {
+		const element = ref.current;
+
+		if (element != null) {
+			setElementTop(element.offsetTop);
+		}
+  }, [ref]);
+
 	return (
 		<section className='TitleSection'>
 			<div className='TitleSection__Splash-Container'>
@@ -11,7 +30,7 @@ const TitleSection: React.FC = () => {
 						<span className='TitleSection__BlockFlow__sub-title'>consensus <br /> and scalable protocol</span>
 						<div className='TitleSection__BlockFlow__box'>for massive adoption</div>
 					</div>
-					<div className='TitleSection__Splash__background-image' />
+					<motion.div ref={ref} className='TitleSection__Splash__background-image' style={{y, scale: 1.3}} />
 				</div>
 			</div>
 			<div className='TitleSection__GetStarted'>
--- src/sections/whatIs/WhatIsSection.tsx
+++ src/sections/whatIs/WhatIsSection.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import './WhatIsSection.css';
+import { motion } from "framer-motion";
+import { useInView } from 'react-intersection-observer'
+
 import SectionTitle from '../../components/sectionTitle/SectionTitle';
 import scalabilityIcon from '../../images/icons/scalability-icon.svg'
 import decentralizationIcon from '../../images/icons/decentralization-icon.svg'
 import pragmatismIcon from '../../images/icons/pragmatism-icon.svg'
 
+const container = {
+	hidden: {
+		opacity: 0,
+	},
+  visible: {
+		opacity: 1,
+		transition: {
+			when: "beforeChildren",
+			staggerChildren: 0.1
+		}
+	}
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
+
 const WhatIsSection: React.FC = () => {
+
+	const [sellingPointsContainerRef, inView] = useInView({
+		rootMargin: '-100px 0px',
+		triggerOnce: true
+	})
+
+	let sellingPoints : ReactElement = <div className='WhatIsSection__selling-points'/>
+
+	if (inView) {
+		sellingPoints =
+			<motion.div className='WhatIsSection__selling-points' variants={container} initial="hidden" animate="visible">
+				<SellingPoint key={1} imagePath={scalabilityIcon} title='Scalability' desc='Innovative sharding algorithm supports cross-shard transactions natively for the first time'/>
+				<SellingPoint key={2} imagePath={decentralizationIcon} title='Decentralization' desc='Platform runs in an open, permission-less network securely, like Bitcoin, only vulnerable to 51% attack'/>
+				<SellingPoint key={3} imagePath={pragmatismIcon} title='Pragmatism' desc='Viable and efficient solutions for scaling smart contract and for confidential transactions'/>
+			</motion.div>
+	}
+
 	return (
 		<section className='WhatIsSection'>
 			<div className='WhatIsSection__description-container'>
@@ -24,10 +65,8 @@ const WhatIsSection: React.FC = () => {
 					</div>
 				</div>
 			</div>
-			<div className='WhatIsSection__selling-points'>
-				<SellingPoint imagePath={scalabilityIcon} title='Scalability' desc='Innovative sharding algorithm supports cross-shard transactions natively for the first time'/>
-				<SellingPoint imagePath={decentralizationIcon} title='Decentralization' desc='Platform runs in an open, permission-less network securely, like Bitcoin, only vulnerable to 51% attack'/>
-				<SellingPoint imagePath={pragmatismIcon} title='Pragmatism' desc='Viable and efficient solutions for scaling smart contract and for confidential transactions'/>
+			<div className='WhatIsSection__selling-points__container' ref={sellingPointsContainerRef}>
+				{sellingPoints}
 			</div>
 		</section>
 	);
@@ -39,13 +78,14 @@ interface SellingPointProps {
 	desc: string
 }
 
+
 const SellingPoint: React.FC<SellingPointProps> = ({ imagePath, title, desc }) => {
 	return (
-		<div className='SellingPoint'>
+		<motion.div className='SellingPoint' variants={item}>
 			<div className='SellingPoint__image' style={{ backgroundImage: `url(${imagePath})` }}/>
 			<div className='SellingPoint__title'>{title}</div>
 			<div className='SellingPoint__desc'>{desc}</div>
-		</div>
+		</motion.div>
 	)
 }
 
