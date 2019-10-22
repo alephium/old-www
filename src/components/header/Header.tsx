9965607e8221b5049b98fdfa7e1817424173c6b5
Mikael Vaivre <m.vaivre@gmail.com>
2019-10-22 16:18:49 +0200

    Adaptive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 import logo from '../../images/logo-h.svg';
+import { motion, useViewportScroll } from 'framer-motion';
 
-const Header: React.FC = () => {
+const headerVariants = {
+  wide: { width: '100%', borderBottomRightRadius: 0, boxShadow: '0', height: 60 },
+  small: { width: '210px', borderBottomRightRadius: 20, boxShadow: '0 15px 15px rgba(0, 0, 0, 0.05)', height: 50},
+}
+
+const logoVariants = {
+  wide: { scale: 1, x: 0 },
+  small: { scale: 0.8, x: -10 },
+}
+
+const spring = {
+  type: "spring",
+  damping: 50,
+  stiffness: 100
+}
+
+const Header = () => {
+	const { scrollY } = useViewportScroll();
+	const [isScrolledDown, setIsScrolledDown] = useState(false)
+
+	useEffect(() => scrollY.onChange(y => setIsScrolledDown(y > 200)), [scrollY])
+
 	return (
-		<header className='Header'>
-			<img src={logo} className="Header__logo" alt="logo" />
-		</header>
+		<motion.header className='Header'
+			animate={isScrolledDown ? "small" : "wide"}
+			variants={headerVariants}
+			transition={spring}
+		>
+			<motion.img src={logo} className="Header__logo" alt="logo"
+				animate={isScrolledDown ? "small" : "wide"}
+				variants={logoVariants}
+				transition={spring}
+			/>
+		</motion.header>
 	);
 }
 
