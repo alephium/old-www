252c1919793b5049c66bef2f3506fbe0a8bd86e5
Mikael Vaivre <m.vaivre@gmail.com>
2019-10-23 15:01:12 +0200

    Apply parallax to main elements in TitleSection

--- src/sections/title/TitleSection.tsx
+++ src/sections/title/TitleSection.tsx
@@ -30,25 +30,27 @@ const TitleSection = () => {
 		<section className='TitleSection'>
 			<div className='TitleSection__Splash-Container'>
 				<div className='TitleSection__Splash'>
-					<div className='TitleSection__BlockFlow'>
+					<ParallaxWrapper className='TitleSection__BlockFlow' movingSpeed={0.2} style={{ skewY: "10deg"}}>
 						<span className='TitleSection__BlockFlow__title'>BlockFlow</span>
 						<span className='TitleSection__BlockFlow__sub-title'>consensus <br /> and scalable protocol</span>
 						<div className='TitleSection__BlockFlow__box'>for massive adoption</div>
+					</ParallaxWrapper>
+					<ParallaxWrapper className={'TitleSection__Splash__background-image'} movingSpeed={0.6} style={{scale: 1.6}} />
+				</div>
+			</div>
+			<ParallaxWrapper className='TitleSection__GetStarted' movingSpeed={-0.4}>
+				<div onMouseMove={handleMouseMove}>
+					<div className='TitleSection__GetStarted__title'>
+						<div className='GetStarted__title__top-label'>Get</div>
+						<div className='GetStarted__title__bottom-label'>Started</div>
 					</div>
-					<ParallaxWrapper className={'TitleSection__Splash__background-image'} movingSpeed={-0.4} style={{scale: 1.6}} />
+					<div className='TitleSection__button-list'>
+						<a href="https://www.alephium.org/docs/white-paper.pdf"><button className='TitleSection__button'>Technical paper</button></a>
+						<a href="https://www.alephium.org/docs/Onepage.pdf"><button className='TitleSection__button'>One pager</button></a>
+						<a href="https://github.com/alephium"><button className='TitleSection__button'>Source repository</button></a>
+					</div>
 				</div>
-			</div>
-			<div className='TitleSection__GetStarted' onMouseMove={handleMouseMove}>
-				<div className='TitleSection__GetStarted__title'>
-					<div className='GetStarted__title__top-label'>Get</div>
-					<div className='GetStarted__title__bottom-label'>Started</div>
-				</div>
-				<div className='TitleSection__button-list'>
-					<a href="https://www.alephium.org/docs/white-paper.pdf"><button className='TitleSection__button'>Technical paper</button></a>
-					<a href="https://www.alephium.org/docs/Onepage.pdf"><button className='TitleSection__button'>One pager</button></a>
-					<a href="https://github.com/alephium"><button className='TitleSection__button'>Source repository</button></a>
-				</div>
-			</div>
+			</ParallaxWrapper>
 		</section>
 	);
 }
