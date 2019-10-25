aed5892864d8ce5694cf262e1fcabbbd5a2dc5ee
Mikael Vaivre <m.vaivre@gmail.com>
2019-10-25 15:42:11 +0200

    Removing refs to swiss/china collaboration

--- src/sections/team/TeamSection.tsx
+++ src/sections/team/TeamSection.tsx
@@ -19,12 +19,6 @@ const TeamSection = () => {
 		<section className='TeamSection' id="team">
 			<ParallaxWrapper className='TeamSection__background' movingSpeed={0.2}/>
 			<SectionTitle title='Team' label='OUR TALENTED' />
-			<div className='TeamSection__collaboration-sticker__wrapper' >
-				<div className='TeamSection__collaboration-sticker' >
-					<p>Richeness of collaboration</p>
-					<p>Switzerland and China</p>
-				</div>
-			</div>
 			<div className='TeamSection__container'>
 				<div className='TeamSection__team-members member-grid'>
 					<TeamMember
