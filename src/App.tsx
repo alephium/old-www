f480876b8ad5d36505136af0c728c25fd97b700a
Cheng Wang <wachmc@gmail.com>
2019-10-29 12:25:03 +0100

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import TitleSection from './sections/title/TitleSection';
 import WhatIsSection from './sections/whatIs/WhatIsSection';
 import FloatingMenu from './components/floatingMenu/FloatingMenu';
 import TechnologyHeader from './sections/technologyHeader/TechnologyHeader';
-import CompetitionSection from './sections/competition/CompetitionSection';
 import FAQSection from './sections/faq/FAQSection';
 import RoadmapSection from './sections/roadmap/RoadmapSection';
 import TeamSection from './sections/team/TeamSection';
