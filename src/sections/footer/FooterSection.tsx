6350fd20f615057ddc3d9199102c311b11b23457
Cheng Wang <wachmc@gmail.com>
2020-10-12 09:58:40 +0200

    Update telegram

--- src/sections/footer/FooterSection.tsx
+++ src/sections/footer/FooterSection.tsx
@@ -7,7 +7,7 @@ const FooterSection = () => {
 			<div className='FooterSection__follow-us'>
 				<h1>Follow us on:</h1>
 				<div className='FooterSection__follow-us__networks'>
-					<a href='https://t.me/joinchat/Jti2ixPvStmspaT1E3I8QQ'><div className='social-icon telegram' /></a>
+					<a href='https://t.me/alephiumgroup'><div className='social-icon telegram' /></a>
 					<a href='https://twitter.com/alephium'><div className='social-icon twitter' /></a>
 					<a href='https://medium.com/@alephium'><div className='social-icon medium' /></a>
 					<a href='https://weibo.com/u/6993237481'><div className='social-icon weibo' /></a>
